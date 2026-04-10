3a5f3205115565f02d052791910894e06362fccb
joaodemutti <joaofacindemutti@gmail.com>
2026-04-10 19:48:35 -0300

    fix: migrations import

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,10 +1,5 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { Usuario } from "./entity/usuario.entity"
-import { Imagem } from "./entity/imagem.entity"
-import { Mensagem } from "./entity/mensagem.entity"
-import { Veiculo } from "./entity/veiculo.entity"
-
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
@@ -14,7 +9,7 @@ export const AppDataSource = new DataSource({
     database: "test",
     synchronize: true,
     logging: false,
-    entities: [Usuario, Imagem, Mensagem, Veiculo],
-    migrations: [],
+    entities: ["src/entity/*.entity{.ts,.js}"],
+    migrations: ["src/migration/*{.ts,.js}"],
     subscribers: [],
 })
