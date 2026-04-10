20de8443a13dc96bb879d971cff50fb96120e5ef
joaodemutti <joaofacindemutti@gmail.com>
2026-04-10 20:36:49 -0300

    refactor: uuid

--- src/migration/1774656359654-migration.ts
+++ src/migration/1774656359654-migration.ts
@@ -4,9 +4,10 @@ export class Migration1774656359654 implements MigrationInterface {
     name = 'Migration1774656359654'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
         await queryRunner.query(`
             CREATE TABLE "Usuario" (
-                "Id" SERIAL NOT NULL,
+                "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "Login" character varying NOT NULL,
                 "Senha" character varying NOT NULL,
                 "Nome" character varying NOT NULL,
@@ -18,9 +19,9 @@ export class Migration1774656359654 implements MigrationInterface {
 
         await queryRunner.query(`
             CREATE TABLE "Veiculo" (
-                "Id" SERIAL NOT NULL,
-                "IdComprador" integer,
-                "IdVendedor" integer NOT NULL,
+                "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "IdComprador" uuid,
+                "IdVendedor" uuid NOT NULL,
                 "Valor" numeric NOT NULL,
                 "Descricao" character varying NOT NULL,
                 "Modelo" character varying NOT NULL,
@@ -40,8 +41,8 @@ export class Migration1774656359654 implements MigrationInterface {
 
         await queryRunner.query(`
             CREATE TABLE "Imagem" (
-                "Id" SERIAL NOT NULL,
-                "IdVeiculo" integer NOT NULL,
+                "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+                "IdVeiculo" uuid NOT NULL,
                 "Conteudo" bytea NOT NULL,
                 "DeletadoEm" TIMESTAMP,
                 "CriadoEm" TIMESTAMP NOT NULL,
@@ -52,11 +53,11 @@ export class Migration1774656359654 implements MigrationInterface {
 
         await queryRunner.query(`
             CREATE TABLE "Mensagem" (
-                "Id" SERIAL NOT NULL,
+                "Id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "Mensagem" character varying NOT NULL,
-                "IdRemetente" integer NOT NULL,
-                "IdDestinatario" integer NOT NULL,
-                "IdVeiculo" integer NOT NULL,
+                "IdRemetente" uuid NOT NULL,
+                "IdDestinatario" uuid NOT NULL,
+                "IdVeiculo" uuid NOT NULL,
                 "CriadoEm" TIMESTAMP NOT NULL,
                 CONSTRAINT "PK_Mensagem" PRIMARY KEY ("Id"),
                 CONSTRAINT "FK_Mensagem_Remetente" FOREIGN KEY ("IdRemetente") REFERENCES "Usuario"("Id"),
@@ -82,5 +83,6 @@ export class Migration1774656359654 implements MigrationInterface {
         await queryRunner.query(`
             DROP TABLE "Usuario";
         `);
+        await queryRunner.query(`DROP EXTENSION IF EXISTS "uuid-ossp"`);
     }
 }
